54d9f808a72054f97e66125d2bfa9d244dc257f6
megatvonline <130200199+megatvonline@users.noreply.github.com>
2023-04-17 18:09:48 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+self.addEventListener('activate', function(event) {
+  var expectedCacheNames = Object.keys(CURRENT_CACHES).map(function(key) {
+    return CURRENT_CACHES[key];
+  });
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (expectedCacheNames.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open(CURRENT_CACHES.font).then(function(cache) {
+      return cache.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request.clone()).then(function(response) {
+          if (response.status < 400) {
+            cache.put(event.request, response.clone());
+          }
+
+          return response;
+        });
+      });
+    })
+  );
+});
+
+self.addEventListener('message', function(event) {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
