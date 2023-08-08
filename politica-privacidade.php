70f1580c5e569e2b21cad48e9dff865c382b3989
megatvonline <130200199+megatvonline@users.noreply.github.com>
2023-08-07 22:27:18 -0300

    Create politica-privacidade.php

---
+++ politica-privacidade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Política de Privacidade - Mega TV Online</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div class="header">
+    <div class="container">
+      <h1>Política de Privacidade</h1>
+    </div>
+  </div>
+  
+  <div class="container">
+    <div class="content">
+      <h2>Coleta de Informações</h2>
+      <p>Mega TV Online valoriza a privacidade dos seus usuários. Esta Política de Privacidade descreve como coletamos, usamos, armazenamos e divulgamos informações pessoais quando você utiliza nosso site.</p>
+      
+      <h2>Informações que Coletamos</h2>
+      <p>Quando você visita o site Mega TV Online, coletamos informações não pessoais, como o tipo de navegador utilizado, o sistema operacional, a data e a hora de acesso, e outras informações técnicas. Esses dados são utilizados para análise estatística e melhorias no nosso site.</p>
+      
+      <p>Além disso, caso você se cadastre em nosso site ou interaja com algum de nossos serviços, poderemos coletar as seguintes informações pessoais:</p>
+      <ul>
+        <li>Nome e sobrenome</li>
+        <li>Endereço de e-mail</li>
+        <li>Informações de contato</li>
+        <li>Informações demográficas</li>
+        <li>Outras informações relevantes para pesquisas e ofertas</li>
+      </ul>
+      
+      <h2>Uso de Informações</h2>
+      <p>As informações coletadas são utilizadas para entender as necessidades e preferências dos nossos usuários, personalizar a experiência de navegação, enviar comunicados, oferecer promoções e melhorar nossos produtos e serviços.</p>
+      
+      <h2>Compartilhamento de Informações</h2>
+      <p>Mega TV Online não vende, aluga ou divulga suas informações pessoais a terceiros, exceto se formos obrigados por lei ou se obtivermos seu consentimento.</p>
+      
+      <h2>Segurança das Informações</h2>
+      <p>Tomamos medidas de segurança adequadas para proteger suas informações pessoais contra acesso não autorizado, alteração, divulgação ou destruição. No entanto, nenhum método de transmissão pela internet ou armazenamento eletrônico é 100% seguro, e não podemos garantir sua segurança absoluta.</p>
+      
+      <h2>Links para Sites de Terceiros</h2>
+      <p>O nosso site pode conter links para sites de terceiros que não são controlados por nós. Não nos responsabilizamos pela privacidade ou conteúdo presente nesses sites. Recomendamos que você revise as políticas de privacidade deles antes de fornecer qualquer informação pessoal.</p>
+    </div>
+  </div>
+
+  <footer class="footer">
+    <div class="container">
+      <div class="footer-links">
+        <a href="politica-privacidade.php"><b>Política de Privacidade</b></a> |
+        <a href="termos-uso.php"><b>Termos de Uso</b></a> |
+        <a href="dmca.php"><b>DMCA</b></a>
+      </div>
+      <div class="copyright">
+        &copy; <span id="current-year"><b>2023</b></span> <b>Mega TV Online. Todos os direitos reservados</b>.
+      </div>
+    </div>
+  </footer>
+
+</body>
+</html>
